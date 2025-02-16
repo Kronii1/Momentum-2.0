ee9ac055a14662bb40ee4a0cff56754133a5b89b
Kronii1 <bfvkdlxj@naver.com>
2025-02-16 22:26:56 +0900

    #4.3 Events part Two After Study

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
   console.log(username);
 }
 
+function handleLink() {
+  alert("clicked!");
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+
+link.addEventListener("click", handleLink);
