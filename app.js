12034a57acf77b2b4b658f25ae3a6c3b990fb309
Kronii1 <bfvkdlxj@naver.com>
2025-02-16 22:03:17 +0900

    #4.1 Form Submission After Study

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function handleLoginBtnClick() {
-  console.dir(loginInput.value);
+  const username = loginInput.value;
 }
 
 // login button click 감지
