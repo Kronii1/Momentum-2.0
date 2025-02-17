95c6017820ae6703f16c3c5b2287358b3d9ef516
Kronii1 <bfvkdlxj@naver.com>
2025-02-17 21:05:14 +0900

    #4.4 Getting Username After Study

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const loginInput = document.querySelector("#login-form input");
 const loginForm = document.querySelector("#login-form");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   const username = loginInput.value;
-  console.log(username);
-}
-
-function handleLink() {
-  alert("clicked!");
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-
-link.addEventListener("click", handleLink);
