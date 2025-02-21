90cb4431ffe4708dab0e87194677006dd48d0340
Kronii1 <bfvkdlxj@naver.com>
2025-02-21 10:35:40 +0900

    #7.1 Adding Todos After Study

--- js/todo.js
+++ js/todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+function printToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  printToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
