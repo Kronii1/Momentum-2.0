d95cd7849d2f266c5925a421181f060ab0e5d148
Kronii1 <bfvkdlxj@naver.com>
2025-02-21 20:52:51 +0900

    delete bug

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,5 @@
 import { API_KEY } from "./api";
 
-const API_KEY = "0e3d959b66216578eed1d3be1fe0942d";
-
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
