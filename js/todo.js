f1d9481988e464ca06d6ec96cee3c70d409821a4
Kronii1 <bfvkdlxj@naver.com>
2025-02-21 10:50:16 +0900

    #7.3 Saving To Dos After Study

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,30 @@ const toDoForm = document.getElementById("todo-form");
 const toDoList = document.getElementById("todo-list");
 const toDoInput = toDoForm.querySelector("input");
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function printToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+
+  const delButton = document.createElement("button");
+  delButton.innerText = "❌";
+
+  delButton.addEventListener("click", deleteToDo);
+
+  li.appendChild(span);
+  li.appendChild(delButton);
+
   toDoList.appendChild(li);
 }
 
@@ -14,7 +33,9 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  toDos.push(newTodo);
   printToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
